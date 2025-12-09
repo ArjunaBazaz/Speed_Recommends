94032fa4f3866aa27fee6ac795bf3bcf90b0547b
Arjuna Bazaz <arjunabazaz@gmail.com>
2025-12-08 22:29:56 -0500

    added advanced search options

--- core/search/search.py
+++ core/search/search.py
@@ -3,23 +3,27 @@ from core.models.games import Game
 from django.core.paginator import Paginator
 from django.db.models import Q
 
-def search_games_either(query: str, page_number: str):
-    return search_games_keyword(query, page_number)
+def search_games_either(query: str, page_number: str, selected_genre: str, selected_platform: str):
+    return search_games_keyword(query, page_number, selected_genre, selected_platform)
 
-def search_games_keyword(query: str, page_number: str):
-    qs = (
-        Game.objects
-        .only("id", "title")
-        .filter(
+def search_games_keyword(query: str, page_number: str, selected_genre: str, selected_platform: str):
+    
+    qs = Game.objects.only("id", "title")
+
+    if query:
+        qs = qs.filter(
             Q(title__icontains=query) |
             Q(genres__name__icontains=query) |
-            Q(platforms__name__icontains=query) |
-            Q(developers__name__icontains=query)
+            Q(platforms__name__icontains=query)
         )
-        .distinct()
-        .prefetch_related("genres", "platforms", "developers")
-        .order_by("title")
-    )
+
+    if selected_genre:
+        qs = qs.filter(genres__id=selected_genre)
+
+    if selected_platform:
+        qs = qs.filter(platforms__id=selected_platform)
+
+    qs = qs.distinct().order_by("title")
 
     paginator = Paginator(qs, 100)
     return paginator.get_page(page_number)
--- core/views.py
+++ core/views.py
@@ -10,6 +10,10 @@ from core.recommend.likes import toggle_preference
 from core.recommend.utils import recommend_next
 from core.saved.services import save_game_for_user, remove_game_for_user
 from core.search.search import search_games_either
+from core.models.game_info import Genre, Platform
+from django.core.paginator import Paginator
+from django.db.models import Q
+from urllib.parse import urlencode
 
 def home(request):
     if request.user.is_authenticated:
@@ -51,24 +55,57 @@ def search_games(request):
     query = request.GET.get("q", "")
     page_number = request.GET.get("page", 1)
 
-    page_obj = search_games_either(query, page_number)
+    selected_genres = request.GET.getlist("genres")
+    selected_platforms = request.GET.getlist("platforms")
 
-    saved_ids = set()
-    if request.user.is_authenticated:
-        saved_ids = set(
-            SavedGame.objects
-            .filter(user=request.user)
-            .values_list("game_id", flat=True)
+    qs = Game.objects.only("id", "title")
+
+    if query:
+        qs = qs.filter(
+            Q(title__icontains=query) |
+            Q(genres__name__icontains=query) |
+            Q(platforms__name__icontains=query)
         )
+
+    if selected_genres:
+        qs = qs.filter(genres__id__in=selected_genres)
+
+    if selected_platforms:
+        qs = qs.filter(platforms__id__in=selected_platforms)
+
+    qs = qs.distinct().order_by("title")
+
+    paginator = Paginator(qs, 100)
+    page_obj = paginator.get_page(page_number)
+
+    saved_ids = set(
+        SavedGame.objects
+        .filter(user=request.user)
+        .values_list("game_id", flat=True)
+    )
+
+    all_genres = Genre.objects.order_by("name")
+    all_platforms = Platform.objects.order_by("name")
+
+    preserved = request.GET.copy()
+    if "page" in preserved:
+        preserved.pop("page")
+
+    preserved_query = preserved.urlencode()
 
     return render(
         request,
         "search.html",
         {
             "query": query,
-            "page_obj": page_obj,
             "games": page_obj.object_list,
+            "page_obj": page_obj,
             "saved_ids": saved_ids,
+            "genres": all_genres,
+            "platforms": all_platforms,
+            "selected_genres": selected_genres,
+            "selected_platforms": selected_platforms,
+            "preserved_query": preserved_query,
         }
     )
 
